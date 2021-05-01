b87021dfa3782e7bfcb5ffe97a2b3719467bee35
Uvarov Aleksandr <sanyauvarov@mail.ru>
2021-05-01 23:41:38 +0300

    Even or odd

---
+++ src/main/java/my/kata/even_or_odd/EvenOrOdd.java
@@ -0,0 +1,7 @@
+package main.java.my.kata.even_or_odd;
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+}
---
+++ src/main/java/my/kata/even_or_odd/EvenOrOddTest.java
@@ -0,0 +1,19 @@
+package main.java.my.kata.even_or_odd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EvenOrOddTest {
+    @Test
+    public void testEvenOrOdd() {
+        EvenOrOdd eoo = new EvenOrOdd();
+        assertEquals("Even", eoo.even_or_odd(6));
+        assertEquals("Odd", eoo.even_or_odd(7));
+        assertEquals("Even", eoo.even_or_odd(10));
+        assertEquals("Odd", eoo.even_or_odd(17));
+        assertEquals("Even", eoo.even_or_odd(-6));
+        assertEquals("Odd", eoo.even_or_odd(-7));
+        assertEquals("Even", eoo.even_or_odd(0));
+    }
+}
